2f49ff837341ce7df6d572dc3f2b522bb2bf6cc0
default <olivia.ten-den@stu.ocsb.ca>
2025-03-04 17:39:25 +0000

    copied over files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+// Created by: Olivia Ten Den
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  alert('Hello, World!')
+}
