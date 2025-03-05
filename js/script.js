0a061295a71976339e5c797992c3ecb690b0a3b5
default <olivia.ten-den@stu.ocsb.ca>
2025-03-05 18:11:19 +0000

    fixed image tag

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
 // Created by: Olivia Ten Den
-// Created on: Sep 2020
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
